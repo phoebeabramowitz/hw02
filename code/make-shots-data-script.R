52366391c0432908bd8bb77917117ed74cfe75d8
phoebeabramowitz <phoebe.abramowitz@berkeley.edu>
2018-03-05 09:51:27 -0800

    Monday Updates
    More work on Question 3

--- code/make-shots-data-script.R
+++ code/make-shots-data-script.R
@@ -33,4 +33,28 @@ green <- mutate(green,name="Draymond Green")
 thompson <- mutate(thompson,name="Klay Thompson")
 durant <- mutate(durant,name="Kevin Durant")
 
-#try the rest of #3 in office hours
+#change values of shot_made_flag to be more descriptive
+curry[curry$shot_made_flag=="y","shot_made_flag"] = "made shot"
+curry[curry$shot_made_flag=="n","shot_made_flag"] = "missed shot"
+
+iguodala[iguodala$shot_made_flag=="y","shot_made_flag"] = "made shot"
+iguodala[iguodala$shot_made_flag=="n","shot_made_flag"] = "missed shot"
+
+green[green$shot_made_flag=="y","shot_made_flag"] = "made shot"
+green[green$shot_made_flag=="n","shot_made_flag"] = "missed shot"
+
+thompson[thompson$shot_made_flag=="y","shot_made_flag"] = "made shot"
+thompson[thompson$shot_made_flag=="n","shot_made_flag"] = "missed shot"
+
+durant[durant$shot_made_flag=="y","shot_made_flag"] = "made shot"
+durant[durant$shot_made_flag=="n","shot_made_flag"] = "missed shot"
+
+#add column minute number where shot occured
+curry <- mutate(curry,minute=(12*period)-minutes_remaining)
+iguodala <- mutate(iguodala,minute=(12*period)-minutes_remaining)
+green <- mutate(green,minute=(12*period)-minutes_remaining)
+thompson <- mutate(thompson,minute=(12*period)-minutes_remaining)
+durant <- mutate(durant,minute=(12*period)-minutes_remaining)
+
+#use sink()
+
